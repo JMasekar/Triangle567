c9e1d4b8eecb19241e510d9d8d09010b0c9ec8d5
JMasekar <57605443+JMasekar@users.noreply.github.com>
2020-02-05 07:33:56 -0500

    First commit

--- Triangle.py
+++ Triangle.py
@@ -9,7 +9,8 @@ The primary goal of this file is to demonstrate a simple python program to class
 @author: rk
 """
 
-def classifyTriangle(a,b,c):
+
+def classifyTriangle(a, b, c):
     """
     Your correct code goes here...  Fix the faulty logic below until the code passes all of 
     you test cases. 
@@ -27,31 +28,25 @@ def classifyTriangle(a,b,c):
       BEWARE: there may be a bug or two in this code
     """
 
-    # require that the input values be >= 0 and <= 200
-    if a > 200 or b > 200 or c > 200:
-        return 'InvalidInput'
-        
-    if a <= 0 or b <= b or c <= 0:
-        return 'InvalidInput'
-    
-    # verify that all 3 inputs are integers  
-    # Python's "isinstance(object,type) returns True if the object is of the specified type
-    if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
-        return 'InvalidInput';
-      
-    # This information was not in the requirements spec but 
-    # is important for correctness
-    # the sum of any two sides must be strictly less than the third side
-    # of the specified shape is not a triangle
-    if (a >= (b - c)) or (b >= (a - c)) or (c >= (a + b)):
-        return 'NotATriangle'
-        
-    # now we know that we have a valid triangle 
-    if a == b and b == a:
-        return 'Equilateral'
-    elif ((a * 2) + (b * 2)) == (c * 2):
-        return 'Right'
-    elif (a != b) and  (b != c) and (a != b):
-        return 'Scalene'
+    if a > 500 or b > 500 or c > 500:
+        return "Error"
+
+    if a <= 0 or b <= 0 or c <= 0:
+        return "Error"
+
+    if not (isinstance(a, int) and isinstance(b, int) and isinstance(c, int)):
+        return "Error"
+
+    if (a >= (b + c)) or (b >= (a + c)) or (c >= (a + b)):
+        return "It is not a Triangle"
+
+    if a == b and b == c and c == a:
+        return "Equilateral Triangle"
+
+    elif ((a * a) + (b * b)) == (c * c) or ((c * c) + (b * b)) == (a * a) or ((c * c) + (a * a)) == (b * b):
+        return "Right Triangle"
+
+    elif (a != b) and (b != c) and (a != c):
+        return "Scalene Triangle"
     else:
-        return 'Isoceles'
+        return "Isosceles Triangle"
